ed9e1c8cbeef68b5ed0e262649c73110b4e86e25
Andrés Saam <andressaa.dev@gmail.com>
2021-07-25 16:02:22 -0500

    Add restaurant card component with its styles

---
+++ src/components/RestaurantCard/index.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './styles.css';
+
+const RestaurantCard = ({ imgSrc, imgAlt, name, address,  }) => {
+    return (
+        <div className="card">
+            <div className="card__img-container">
+                <img src={imgSrc} alt={imgAlt} />
+            </div>  
+            <div className="card__description">
+                <h2>{name}</h2>
+                <p>{address}</p>
+            </div>
+        </div>
+    )
+}
+
+export default RestaurantCard
