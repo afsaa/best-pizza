dac9068cc84ccb22d2b869c182946d4a161ef071
Andrés Saam <andressaa.dev@gmail.com>
2021-07-26 23:15:11 -0500

    Add axios for async requests and react router dom

--- src/App.js
+++ src/App.js
@@ -1,63 +1,25 @@
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import PanosPizza from './assets/Panos_pizza.png'
-import SbarroPizza from './assets/Sbarro.png'
-import RestaurantCard from "./components/RestaurantCard";
-import './App.scss'
+import React from 'react'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
 
-const pizzerias = [{
-  name: "Pano's Pizza",
-  address: "Av. Siempre Viva 123",
-  imgSrc: PanosPizza,
-  imgAlt: "Pano's pizza",
-}, {
-  name: "Sbarro Pizza",
-  address: "Av. Siempre Viva 123",
-  imgSrc: SbarroPizza,
-  imgAlt: "Sbarro pizza",
-}, {
-  name: "Sbarro Pizza",
-  address: "Av. Siempre Viva 123",
-  imgSrc: SbarroPizza,
-  imgAlt: "Sbarro pizza",
-}, {
-  name: "Sbarro Pizza",
-  address: "Av. Siempre Viva 123",
-  imgSrc: SbarroPizza,
-  imgAlt: "Sbarro pizza",
-}, {
-  name: "Sbarro Pizza",
-  address: "Av. Siempre Viva 123",
-  imgSrc: SbarroPizza,
-  imgAlt: "Sbarro pizza",
-}]
+import Login from "./pages/Login";
+import Home from "./pages/Home";
 
 function App() {
   return (
-    <div className="app">
-      <Header />
-      <div className="main">
-        <nav className="nav">
-          <ul className="nav__menu">
-            <li className="menu__item">Pizzerías</li>
-          </ul>
-          <div>
-            <p>Salir</p>
-          </div>
-        </nav>
-        <div className="main__description">
-          <h1>Tiendas</h1>
-          <p>Escoge tu pizzería favorita</p>
-        </div>
-        <div className="main__container">
-          <div className="pizzerias__container">
-            {pizzerias.map((restaurant, index) => {
-            return (<RestaurantCard key={index} name={restaurant.name} address={restaurant.address} imgSrc={restaurant.imgSrc} imgAlt={restaurant.imgAlt} />)})}
-          </div>
-        </div>
-      </div>
-      <Footer />
-    </div>
+    <Router>
+        <Switch>
+          <Route exact path="/">
+            <Login />
+          </Route>
+          <Route path="/home">
+            <Home />
+          </Route>
+        </Switch>
+    </Router>
   );
 }
 
